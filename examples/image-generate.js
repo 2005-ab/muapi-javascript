3638176be9905982627bf37fff004650072fed42
Abhi <abhi15sood@gmail.com>
2026-06-09 11:03:54 +0530

    docs(examples): remove hardcoded api keys

--- examples/image-generate.js
+++ examples/image-generate.js
@@ -1,9 +1,8 @@
 import { MuAPI } from "../src/index.js";
 
 const client = new MuAPI(
-    "e17689ccf2a52e494b5be5a7dfb3508f63f0f8b19210f3459cf2f5f8f65cbbb5"
+    "your-api-key"
 );
-
 const result =
     await client.images.edit({
         prompt:
